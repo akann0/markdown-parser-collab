1c4f7dfe3fe45c059546f1073445d72543c7e03c
JL-Young <jlyoung@ucsd.edu>
2022-04-21 11:38:32 -0700

    Lab Week 4 Part 6

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -1,9 +1,46 @@
 import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.beans.Transient;
+import java.nio.file.Files;
+import java.util.ArrayList;
+
 import org.junit.*;
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
     }
-}
 
+    @Test
+    public void getLinksTest() throws IOException {
+        boolean result = false;
+        Path fileName = Path.of("testFile.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        if (links.contains("https://something.com") && 
+            links.contains("some-thing.html")) {
+                result = true;
+            }
+
+        assertTrue(result);
+    }
+
+    @Test
+    public void testFile() throws IOException {
+        boolean result = false;
+        Path fileName = Path.of("test-file.md");
+        String content = Files.readString(fileName);
+        ArrayList<String> links = MarkdownParse.getLinks(content);
+
+        if (links.contains("https://something.com") && 
+            links.contains("some-thing.html")) {
+                result = true;
+            }
+
+        assertTrue(result);
+    }
+}
